c59f9d5d0fd9cc954fd987b766864196678b406a
SANTIAGOBCF <58187994+SANTIAGOBCF@users.noreply.github.com>
2022-12-22 03:56:34 -0500

    endpoint image and returns url

---
+++ imageApp/models.py
@@ -0,0 +1,7 @@
+"""
+from django.db import models
+
+
+class imageEndpoint(models.Model):
+    imageUploaded = models.FileField()
+"""
--- memorex/settings.py
+++ memorex/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'cloudinary',
     'politician',
     'post',
     'user',
