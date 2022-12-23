64897fd077a8172296ab03d6f3e67d7162741677
SANTIAGOBCF <58187994+SANTIAGOBCF@users.noreply.github.com>
2022-12-22 19:41:04 -0500

    image_uploader

--- imageApp/models.py
+++
@@ -1,7 +0,0 @@
-"""
-from django.db import models
-
-
-class imageEndpoint(models.Model):
-    imageUploaded = models.FileField()
-"""
--- memorex/api.py
+++ memorex/api.py
@@ -3,7 +3,7 @@ from ninja import NinjaAPI
 from politician.api import router as politician_router
 from post.api import router as post_router
 from user.api import router as user_router
-from imageApp.api import router as image_router
+from image_uploader.api import router as image_router
 
 from .constants import Routers
 
